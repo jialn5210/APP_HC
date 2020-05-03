32d1fc995f679eb550d124c0e163dfc5236022bb
catarinafonseca <catarinaconstantef@gmail.com>
2020-05-03 16:57:50 +0100

    grafico

--- covid.js
+++ covid.js
@@ -18,3 +18,31 @@ counters.forEach(counter => {
 
 	updateCount();
 });
+
+
+jQuery(document).ready(function() {
+	var chartDiv = $("#barChart");
+	var myChart = new Chart(chartDiv, {
+		type: 'pie',
+		data: {
+			labels: ["Confirmados", "Recuperados", "Óbitos", "Suspeitos"],
+			datasets: [
+			{
+				data: [12,7, 3, 75],
+				backgroundColor: [
+				   "#00B0F2",
+				"#18F200",
+				"#FF0606",
+				"#FF9B06"
+				]
+			}]
+		},
+		options: {
+			responsive: true,
+	maintainAspectRatio: false,
+		}
+	});
+		});
+	
+	
+	
