4b87f133618033a90b5d1e5497c2fb4f048ad6a8
jialn5210 <jialongye5210@gmail.com>
2020-05-10 16:09:56 +0100

    update

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ floating_btn.addEventListener('click', () => {
 close_btn.addEventListener('click', () => {
 	social_panel_container.classList.remove('visible')
 });
+
+$(window).on('scroll',function(){
+    if($(window).scrollTop()){
+        $('header').addClass('black');
+
+    }
+    else{
+        $('header').removeClass('black');
+    }
+})
+new WOW().init();
+
+
+$(".counter").counterUp({delay:10,time:1000});
+
+$(document).ready(function(){
+    $(".menu .fa-bars").click(function(){
+        $("nav ul").toggleClass("active")
+    })
+})
+$(document).ready(function(){
+ $('.carousel').carousel();
+ });
+
+
+ var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+  });
