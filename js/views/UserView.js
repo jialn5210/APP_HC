bd6aef5b310404de1b6771cefb89f9d122e22e44
jialn5210 <jialongye5210@gmail.com>
2020-05-29 14:53:41 +0100

    username nao passa

--- js/views/UserView.js
+++ js/views/UserView.js
@@ -5,7 +5,7 @@ import UserController from '../controllers/UserController.js'
 export default class UserView {
     constructor() {
         this.userController = new UserController();
-        
+
         // register DOM
         this.registerForm = document.getElementById('frmRegister');
         this.registerUsername = document.getElementById('txtUsernameRegister');
@@ -21,7 +21,7 @@ export default class UserView {
         this.loginUsername = document.getElementById('txtUsername');
         this.loginPassword = document.getElementById('txtPassword');
         this.loginMessage = document.getElementById('mdlLoginMessage');
-        
+
 
         this.bindAddLoginForm();
 
@@ -34,125 +34,125 @@ export default class UserView {
 
         this.checkLoginStatus();
 
-        
-        
-        
-// buttons Social painel
+
+
+
+        // buttons Social painel
         this.floating_btn = document.querySelector('.floating-btn');
         this.close_btn = document.querySelector('.close-btn');
         this.social_panel_container = document.querySelector('.social-panel-container');
-         
+
         this.bindAddFloating_btn();
         this.bindAddClose_btn();
 
-    //counter
-        this.Counter=document.querySelector('.counter');
+        //counter
+        this.Counter = document.querySelector('.counter');
         this.Counters();
     }
 
-    
-    bindAddFloating_btn(){
+
+    bindAddFloating_btn() {
         this.floating_btn.addEventListener('click', () => {
             this.social_panel_container.classList.toggle('visible')
         });
     }
-    bindAddClose_btn(){
-        this.close_btn.addEventListener('click',() => {
+    bindAddClose_btn() {
+        this.close_btn.addEventListener('click', () => {
             this.social_panel_container.classList.remove('visible')
-        
+
         });
     }
-    Counters(){
-        this.Counter.counterUp({delay:10,time:1000});
+    Counters() {
+        this.Counter.counterUp({ delay: 10, time: 1000 });
     }
-    
-  
-   
-       
-        
-    
 
-bindAddRegisterForm() {
-    this.registerForm.addEventListener('submit', event => {
-        event.preventDefault();
 
-        try {
-            if (this.registerPassword.value !== this.registerPassword2.value) {
-                throw Error('Password and Confirm Password are not equal');
+
+
+
+
+
+    bindAddRegisterForm() {
+        this.registerForm.addEventListener('submit', event => {
+            event.preventDefault();
+
+            try {
+                if (this.registerPassword.value !== this.registerPassword2.value) {
+                    throw Error('Password and Confirm Password are not equal');
+                }
+                this.userController.createUser(this.registerUsername.value, this.registerPassword.value);
+                this.displayRegisterMessage('User registered with success!', 'success');
+            } catch (e) {
+                this.displayRegisterMessage(e, 'danger');
             }
-            this.userController.createUser(this.registerUsername.value, this.registerPassword.value);
-            this.displayRegisterMessage('User registered with success!', 'success');
-        } catch (e) {
-            this.displayRegisterMessage(e, 'danger');
-        }
-    });
-}
-
-bindAddLoginForm() {
-    this.loginForm.addEventListener('submit', event => {
-        event.preventDefault();
-
-        try {
-            this.userController.loginUser(this.loginUsername.value, this.loginPassword.value);
-            this.displayLoginMessage('User logged in with success!', 'success');
-
-            // Wait 1 second before reloading, so the user can see the login success message
-            setTimeout(() => {
-                this.updateButtons('login');
-                location.replace('html/hc.html')
-
-              
-
-            },
-                1000);
-
-        } catch (e) {
-            this.displayLoginMessage(e, 'danger');
-        }
-    });
-}
-
-bindAddLogoutEvent() {
-    this.logoutButton.addEventListener('click', event => {
-        this.userController.logoutUser();
-        this.updateButtons('logout');
-        location.reload()
-    });
-}
-
-checkLoginStatus() {
-    if (this.userController.checkLoginStatus()) {
-        this.updateButtons('login');
-    } else {
-        this.updateButtons('logout');
+        });
     }
-}
 
-displayRegisterMessage(message, type) {
-    this.registerMessage.innerHTML =
-        `<div class="alert alert-${type}" role="alert">${message}</div>`;
-}
+    bindAddLoginForm() {
+        this.loginForm.addEventListener('submit', event => {
+            event.preventDefault();
 
-displayLoginMessage(message, type) {
-    this.loginMessage.innerHTML =
-        `<div class="alert alert-${type}" role="alert">${message}</div>`;
-}
+            try {
+                this.userController.loginUser(this.loginUsername.value, this.loginPassword.value);
+                this.displayLoginMessage('User logged in with success!', 'success');
 
-updateButtons(event) {
-    switch (event) {
-        case 'login':
-            this.loginButton.style.visibility = 'hidden'
+                // Wait 1 second before reloading, so the user can see the login success message    
+                setTimeout(() => {
+                    this.updateButtons('login');
+                    location.replace('html/hc.html')
+                    document.getElementById('user').innerHTML = this.loginUsername.value;
 
-            this.logoutButton.style.visibility = 'visible'
-            break;
-        case 'logout':
-            this.loginButton.style.visibility = 'visible'
 
-            this.logoutButton.style.visibility = 'hidden'
+                },
+                    1000);
+
+            } catch (e) {
+                this.displayLoginMessage(e, 'danger');
+            }
+        });
     }
-}
- 
-    
+
+    bindAddLogoutEvent() {
+        this.logoutButton.addEventListener('click', event => {
+            this.userController.logoutUser();
+            this.updateButtons('logout');
+            location.reload()
+        });
+    }
+
+    checkLoginStatus() {
+        if (this.userController.checkLoginStatus()) {
+            this.updateButtons('login');
+        } else {
+            this.updateButtons('logout');
+        }
+    }
+
+    displayRegisterMessage(message, type) {
+        this.registerMessage.innerHTML =
+            `<div class="alert alert-${type}" role="alert">${message}</div>`;
+    }
+
+    displayLoginMessage(message, type) {
+        this.loginMessage.innerHTML =
+            `<div class="alert alert-${type}" role="alert">${message}</div>`;
+    }
+
+    updateButtons(event) {
+        switch (event) {
+            case 'login':
+                this.loginButton.style.visibility = 'hidden'
+
+                this.logoutButton.style.visibility = 'visible'
+                break;
+            case 'logout':
+                this.loginButton.style.visibility = 'visible'
+
+                this.logoutButton.style.visibility = 'hidden'
+        }
+    }
+
+
 }
 
 //background animation
