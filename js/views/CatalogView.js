07eed1ee513985e4410441c16f567f4356cb3241
jialn5210 <jialongye5210@gmail.com>
2020-06-14 15:34:24 +0100

    ....

--- js/views/CatalogView.js
+++ js/views/CatalogView.js
@@ -9,16 +9,17 @@ export default class DoctorView {
         this.catalog = document.querySelector("#myCatalog")
         this.btnFilter = document.querySelector("#btnFilter")
         this.btnSort = document.querySelector("#btnSort")
-        
+        this.spec =document.querySelector("#slt")
         this.txtDoctor = document.querySelector("#txtDoctor")
         this.sltSpecialty = document.querySelector("#sltSpecialty")
 
         this.renderCatalog(this.doctorController.getDoctors())
+        
         this.bindAddFilterEvent()
         this.bindAddSortEvent()
         
     }
-
+  
     bindAddFilterEvent() {
         this.btnFilter.addEventListener('click', () => {            
             this.renderCatalog(this.doctorController.getDoctors(this.txtDoctor.value, this.sltSpecialty.value))
@@ -61,11 +62,11 @@ export default class DoctorView {
 
     _generateDoctorCard(doctor) {
         let html = `
-        <div class="col-sm-4">
-            <div class="card">
-                <img class="card-img-top" src="${doctor.photo}" alt="">
+        <div class="col-sm-4 card ">
+            <div class="card-all">
+                <img class="card-img" src="${doctor.photo}" alt="">
                 <div class="card-body">
-                    <h4 class="card-title">${doctor.name}</h4>
+                    <h4 class="card-title text-center">${doctor.name}</h4>
                     <p class="card-text">${doctor.specialty}</p>
                     <button id="${doctor.id}" class="btn btn-primary see">See more</button>           
                 </div>
